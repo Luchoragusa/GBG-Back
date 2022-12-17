5d5bea2408c6cd01c4b22caed4d118c3508cdb9c
Luchoragusa <luchito.rc4@gmail.com>
2022-12-17 17:04:54 -0300

    remove path and update .routes

---
+++ src/app/modules/admin/autopart/autopart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-autopart',
+  templateUrl: './autopart.component.html',
+  styleUrls: ['./autopart.component.scss']
+})
+export class AutopartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/admin/autopart/autopart.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Route, RouterModule } from '@angular/router';
+import { AutopartComponent } from './autopart.component';
+
+const autopartRoutes: Route[] = [
+  {
+      path     : '',
+      component: AutopartComponent
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(autopartRoutes) 
+  ]
+})
+export class AutopartModule { }
