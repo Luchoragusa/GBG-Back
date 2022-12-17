744cc177772792e1418b58159d85e0797984985d
Luchoragusa <luchito.rc4@gmail.com>
2022-12-17 16:12:33 -0300

    remove search

--- src/app/layout/layouts/compact/compact.module.ts
+++ src/app/layout/layouts/compact/compact.module.ts
@@ -11,7 +11,6 @@ import { FuseNavigationModule } from '@fuse/components/navigation';
 import { LanguagesModule } from 'app/layout/common/languages/languages.module';
 import { MessagesModule } from 'app/layout/common/messages/messages.module';
 import { NotificationsModule } from 'app/layout/common/notifications/notifications.module';
-import { SearchModule } from 'app/layout/common/search/search.module';
 import { ShortcutsModule } from 'app/layout/common/shortcuts/shortcuts.module';
 import { UserModule } from 'app/layout/common/user/user.module';
 import { SharedModule } from 'app/shared/shared.module';
@@ -34,7 +33,6 @@ import { CompactLayoutComponent } from 'app/layout/layouts/compact/compact.compo
         LanguagesModule,
         MessagesModule,
         NotificationsModule,
-        SearchModule,
         ShortcutsModule,
         UserModule,
         SharedModule
