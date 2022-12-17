500c0db728d58aa70488c2023aa5ea184cd479b0
Luchoragusa <luchito.rc4@gmail.com>
2022-12-17 17:39:34 -0300

    add table material

--- src/app/modules/admin/autopart/autopart.module.ts
+++ src/app/modules/admin/autopart/autopart.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Route, RouterModule } from '@angular/router';
 import { AutopartComponent } from './autopart.component';
 
+
 const autopartRoutes: Route[] = [
   {
       path     : '',
