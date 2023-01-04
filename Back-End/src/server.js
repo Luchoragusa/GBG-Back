83696d482ab42996817e48fa5d6621af8faf62e4
Luchoragusa <luchito.rc4@gmail.com>
2023-01-03 23:03:49 -0300

    fix public route images

--- Back-End/src/server.js
+++ Back-End/src/server.js
@@ -19,7 +19,7 @@ app.use(json());;
 app.use('/', router);
 
 // Ruta publica
-app.use(express.static('./uploads'));
+app.use(express.static('./GBG-data/images'));
 
 app.use((req, res, next) => {
   res.status(404).json({
