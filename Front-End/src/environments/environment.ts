e85023300749c27b9a536213efc05e17ef3bc33f
Luchoragusa <luchito.rc4@gmail.com>
2023-04-13 20:58:32 -0300

    3001 -> 1812

--- Front-End/src/environments/environment.ts
+++ Front-End/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
     production: false,
-    apiUrl: "http://186.13.28.61:3001"
-    // apiUrl: "http://localhost:3001"
+    apiUrl: "http://186.13.28.61:1812"
+    // apiUrl: "http://localhost:1812"
 };
 
 /*
