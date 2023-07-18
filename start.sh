60fb2f42bb0de377991a79aea23abbb3befe89f9
Luchoragusa <luchito.rc4@gmail.com>
2023-07-18 19:43:36 -0300

    dev -> main

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ rm -drf ./node_modules
 git pull origin main
 chmod +x ./start.sh
 npm i
-pm2 delete GBG
-pm2 start src/app.js --name GBG
+pm2 delete GBG-Back
+pm2 start src/app.js --name GBG-Back
 pm2 ls
